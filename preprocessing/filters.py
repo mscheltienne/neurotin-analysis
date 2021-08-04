fab59b8ec0ee1a2571b8d7b833302811f7d82622
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-08-04 16:20:42 +0200

    Added CAR, BP and notch filters.

---
+++ preprocessing/filters.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+
+def _check_bandpass(bandpass):
+    if not isinstance(bandpass, (tuple, list)):
+        raise TypeError
+    if len(bandpass) != 2:
+        raise ValueError
+    for fq in bandpass:
+        if fq is not None and fq <= 0:
+            raise ValueError
+
+
+def apply_filter(raw, car, bandpass, notch):
+    """
+    Apply filters:
+        - CAR
+        - Bandpass
+        - Notch
+
+    Parameters
+    ----------
+    raw : Raw
+        Raw instance modified.
+    car : bool
+        If True, a CAR projector is added for the EEG channels.
+    bandpass : tuple
+        A 2-length tuple (highpass, lowpass), e.g. (1., 40.).
+        The lowpass of highpass filter can be disabled by using None.
+        The filter is applied on EEG, EOG and ECG channels.
+    notch : bool
+        If True, a notch filter at (50, 100, 150) Hz is applied on EEG, EOG
+        and ECG channels.
+
+    Returns
+    -------
+    raw : Raw
+    """
+    _check_bandpass(bandpass)
+
+    # Common average reference
+    if car:
+        raw.set_eeg_reference(
+            ref_channels='average', ch_type='eeg', projection=True)
+
+    # Bandpass filter
+    raw.filter(
+        l_freq=bandpass[0], h_freq=bandpass[1], picks=['ecg', 'eog', 'eeg'],
+        method='iir', iir_params=dict(order=4, ftype='butter', output='sos'))
+
+    # Notch filters
+    raw.notch_filter(np.arange(50, 151, 50), picks=['eeg', 'ecg', 'eog'])
+
+    return raw
