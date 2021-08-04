ab923d36221f343a0a77b1cfa174c2165a44311f
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-08-04 15:26:45 +0200

    Added better ticks position for X/Y axis and remove early duplicates of failed MML attempts.

--- logs/mml.py
+++ logs/mml.py
@@ -2,6 +2,7 @@
 The MML are logged with pandas in a .csv file with the syntax:
     [participant, session, MML volume]
 """
+import numpy as np
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
@@ -35,12 +36,16 @@ def plot_mml_evolution_per_participants(csv, participants, figsize=(10, 5)):
         df, id_vars=('Participant', 'Session'), value_vars='MML Volume',
         var_name='MML', value_name='Volume')
     df = df[df['Participant'].isin(participants)]
+    df.drop_duplicates(
+        subset=('Participant', 'Session'), keep='last', inplace=True)
 
     # Plot
     f, ax = plt.subplots(1, 1, figsize=tuple(figsize))
     sns.lineplot(
         x='Session', y='Volume', hue='Participant', data=df,
         palette='muted', style="Participant", markers=True, dashes=False)
+    ax.set_xticks(range(1, 16, 1))
+    ax.set_yticks(np.arange(0, max(df.Volume)+2.5, 2.5))
 
     return f, ax
 
