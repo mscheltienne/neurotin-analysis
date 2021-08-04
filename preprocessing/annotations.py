b0891846d2238cba3960453f8194283a2abd5fb8
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-08-04 16:02:59 +0200

    Added annotations from trigger events.

---
+++ preprocessing/annotations.py
@@ -0,0 +1,44 @@
+import mne
+
+
+EVENTS_MAPPING = {
+    1: 'rest',
+    2: 'blink',
+    3: 'resting-state',
+    4: 'audio',
+    5: 'regulation',
+    6: 'non-regulation'
+}
+EVENTS_DURATION_MAPPING = {
+    1: 1,
+    2: 60,
+    3: 120,
+    4: 0.8,
+    5: 16,
+    6: 8
+}
+
+
+def add_annotations_from_events(raw):
+    """
+    Add annotations from events to the raw instance.
+
+    Parameters
+    ----------
+    raw : Raw
+        Raw instance modified.
+
+    Returns
+    -------
+    raw : Raw
+    annotations : Annotations
+    """
+    events = mne.find_events(raw, stim_channel='TRIGGER')
+    annotations = mne.annotations_from_events(
+        events=events, event_desc=EVENTS_MAPPING, sfreq=raw.info['sfreq'])
+    for k in range(events.shape[0]):
+        idx = events[k, 2]
+        annotations.duration[k] = EVENTS_DURATION_MAPPING[idx]
+        raw.set_annotations(annotations)
+
+    return raw, annotations
