4a1aa8049d4a97f9c830359caae0871f5f29a819
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-09-03 13:19:03 +0200

    Added an exclusion system to list files where an AssertionError is raised.

--- preprocessing/cli.py
+++ preprocessing/cli.py
@@ -4,9 +4,36 @@ from pathlib import Path
 from pipeline import preprocessing_pipeline
 
 
-FOLDER_IN = Path(r"/Volumes/NeuroTin-EEG/Data/Participants")
-FOLDER_OUT = Path(r"/Volumes/NeuroTin-EEG/Data preprocessed/")
+FOLDER_IN = Path(r"/Users/scheltie/Documents/NeuroTin Data/Raw/")
+FOLDER_OUT = Path(r"/Users/scheltie/Documents/NeuroTin Data/Clean/")
 RETRIES = 3
+
+
+def main():
+    """
+    Main preprocessing pipeline, called once per participant.
+    """
+    _, participant_folder = input_participant()
+    dirname_in = FOLDER_IN / participant_folder
+    dirname_out = FOLDER_OUT / participant_folder
+    assert dirname_in.exists()
+    os.makedirs(dirname_out, exist_ok=True)
+    exclude = read_exclusion()
+
+    fifs = list_raw_fif(dirname_in)
+    for fif_in in fifs:
+        fif_out = dirname_out / fif_in.relative_to(dirname_in)
+        if fif_out.exists() or fif_out in exclude:
+            continue
+        os.makedirs(fif_out.parent, exist_ok=True)
+        print("-------------------------------------------------------------")
+        print(f"Preprocesing {fif_in.relative_to(dirname_in)}")
+        try:
+            raw = preprocessing_pipeline(fif_in)
+        except AssertionError:
+            exclude.append(fif_out)
+            write_exclusion(fif_out)
+        raw.save(fif_out, fmt="double")
 
 
 def input_participant():
@@ -64,28 +91,45 @@ def list_raw_fif(directory):
     return fifs
 
 
-def main():
+def read_exclusion():
     """
-    Main preprocessing pipeline, called once per participant.
-    """
-    _, participant_folder = input_participant()
-    dirname_in = FOLDER_IN / participant_folder
-    dirname_out = FOLDER_OUT / participant_folder
-    assert dirname_in.exists()
-    if not dirname_out.exists():
-        os.makedirs(dirname_out)
+    Read the list of output fif files to exclude from disk.
+    If the file storing the exlusion list does not exist, it is created at
+    'FOLDER_OUT / exlusion.txt'.
 
-    fifs = list_raw_fif(dirname_in)
-    for fif_in in fifs:
-        fif_out = dirname_out / fif_in.relative_to(dirname_in)
-        if fif_out.exists():
-            continue
-        if not fif_out.parent.exists():
-            os.makedirs(fif_out.parent)
-        print("-------------------------------------------------------------")
-        print(f"Preprocesing {fif_in.relative_to(dirname_in)}")
-        raw = preprocessing_pipeline(fif_in)
-        raw.save(fif_out, fmt="double")
+    Returns
+    -------
+    exclude : list
+        List of file to exclude.
+    """
+    fname = FOLDER_OUT / 'exlusion.txt'
+    if fname.exists():
+        with open(fname, 'r') as file:
+            exclude = file.readlines()
+        exclude = [line.rstrip() for line in exclude if len(line) > 0]
+    else:
+        with open(fname, 'w'):
+            pass
+        exclude = list()
+    return exclude
+
+
+def write_exclusion(fif_out):
+    """
+    Add a file to the exclusion file 'FOLDER_OUT / exlusion.txt'.
+
+    Parameters
+    ----------
+    fif_out : str | Path
+        Path to the output file to exclude.
+    """
+    fname = FOLDER_OUT / 'exlusion.txt'
+    if not fname.exists():
+        mode = 'w'
+    else:
+        mode = 'a'
+    with open(fname, mode) as file:
+        file.write(str(fif_out) + '\n')
 
 
 if __name__ == "__main__":
