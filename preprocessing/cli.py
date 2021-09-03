1a23463cddb07a1d13b54c81279715dc77880029
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-09-03 14:02:47 +0200

    Added function for yes/no query.

--- preprocessing/cli.py
+++ preprocessing/cli.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from pathlib import Path
 
 from pipeline import preprocessing_pipeline
@@ -89,6 +90,48 @@ def list_raw_fif(directory):
         elif elt.name.endswith("-raw.fif"):
             fifs.append(elt)
     return fifs
+
+
+def query_yes_no(question, default="yes", retries=RETRIES):
+    """
+    Ask a yes/no question via input() and return their answer.
+
+    Parameters
+    ----------
+    question : str
+        String that is presented to the user.
+    default : str
+        Presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+    retries : int
+        Number of invalid input that can be given until an error is raised.
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
+
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("Invalid default answer: '%s'" % default)
+
+    attempt = 1
+    while attempt <= RETRIES:
+        choice = input("[IN] " + question + prompt).lower()
+        if default is not None and choice == "":
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n")
+        attempt += 1
+    else:
+        raise ValueError
 
 
 def read_exclusion():
