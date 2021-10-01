0e439d22b095277671b1276d11b50c1905db87f7
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-10-01 13:56:37 +0200

    Added rejection and repair of epochs with autoreject.

--- time-frequency/utils.py
+++ time-frequency/utils.py
@@ -1,5 +1,6 @@
 import mne
 import numpy as np
+from autoreject import AutoReject, get_rejection_threshold
 
 
 EVENTS = {
@@ -29,7 +30,7 @@ EVENTS_DURATION_MAPPING = {
 EPOCH_EVENTS = {5: 1, 6:2}
 
 
-def make_fixed_length_epochs(raw, duration=1.):
+def make_fixed_length_epochs(raw, duration=1., overlap=0.):
     """
     Create fixed length epochs for neurofeedback runs.
 
@@ -38,7 +39,9 @@ def make_fixed_length_epochs(raw, duration=1.):
     raw : Raw
         Preprocessed raw instance.
     duration : float
-        Duration of an epoch.
+        Duration of an epoch in seconds.
+    overlap : float
+        Duration of epoch overlap in seconds.
 
     Returns
     -------
@@ -57,7 +60,7 @@ def make_fixed_length_epochs(raw, duration=1.):
             stop += 7  # first rest phase extension
         epoch_events = mne.make_fixed_length_events(
             raw, id=EPOCH_EVENTS[event[2]], start=start, stop=stop,
-            duration=duration, first_samp=False, overlap=0.)
+            duration=duration, first_samp=False, overlap=overlap)
         raw.add_events(epoch_events, stim_channel='STI', replace=False)
 
     events = mne.find_events(raw, stim_channel='STI')
@@ -66,3 +69,53 @@ def make_fixed_length_epochs(raw, duration=1.):
                         preload=True, reject=None, flat=None)
 
     return epochs
+
+
+def reject_epochs(epochs, reject=None):
+    """
+    Reject bad epochs with a global rejection threshold.
+
+    Parameters
+    ----------
+    epochs : Epochs
+        Raw epochs, before peak-to-peak rejection.
+    reject : dict | None
+        MNE-compatible rejection dictionary or None to compute it with
+        autoreject.
+
+    Returns
+    -------
+    epochs : Epochs
+        Good epochs.
+    """
+    if reject is None:
+        reject = get_rejection_threshold(epochs, decim=1)
+    epochs.drop_bad(reject=reject)
+    return epochs
+
+
+def repair_epochs(epochs, thresh_method='random_search'):
+    """
+    Repair bad epochs using autoreject.
+
+    Parameters
+    ----------
+    epochs : Epochs
+        Epochs used to fit the rejection model and to repair with the model.
+    thresh_method : str
+        Either 'random_search' or 'bayesian_optimization'.
+
+    Returns
+    -------
+    epochs : Epochs
+        Epochs repaired by the model.
+    """
+    n_interpolates = np.array([1, 4, 32])
+    consensus_percs = np.linspace(0, 1.0, 11)
+
+    picks = mne.pick_types(epochs.info, eeg=True, exclude=[])
+    ar = AutoReject(n_interpolates, consensus_percs, picks=picks,
+                    thresh_method=thresh_method)
+
+    ar.fit(epochs)
+    return ar.transform(epochs)
