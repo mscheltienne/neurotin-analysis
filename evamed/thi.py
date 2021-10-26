615ad701acddccb54b589efcd6348b53ba497cc3
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-10-26 14:16:54 +0200

    Generalization of THI parser for all files containing THI data.

--- evamed/thi.py
+++ evamed/thi.py
@@ -3,18 +3,26 @@ import pandas as pd
 
 def _parse_thi(df, participant):
     """Parse dataframe and extract THI answers and information."""
+    # clean-up columns
     thi_columns = [col for col in df.columns if 'THI' in col]
     assert len(thi_columns) != 0, 'THI not present in dataframe.'
+    prefix = set(col.split('_')[0] for col in thi_columns)
+    assert len(prefix) == 1
+    prefix = list(prefix)[0]
+
+    # locate participant lines
     df = df.loc[df['patient_code'] == participant]
 
+    # extract information
     valid_answers = {'No': 0, 'Sometimes': 2, 'Yes': 4}
-    df_thi = df[[f'THIB_THI{k}' for k in range(1, 26)]]\
+    df_thi = df[[f'{prefix}_THI{k}' for k in range(1, 26)]]\
         .replace(valid_answers)
-    df_thi.rename(mapper={f'THIB_THI{k}': f'Q{k}' for k in range(1, 26)},
+    df_thi.rename(mapper={f'{prefix}_THI{k}': f'Q{k}' for k in range(1, 26)},
                   axis='columns', copy=False, inplace=True)
-    df_thi.insert(0, 'date', pd.to_datetime(df.THIB_date))
-    df_thi.insert(1, 'results', df.THIB_THI_R)
+    df_thi.insert(0, 'date', pd.to_datetime(df[f'{prefix}_date']))
+    df_thi.insert(1, 'results', df[f'{prefix}_THI_R'])
 
+    # sanity-check
     assert (df_thi[[f'Q{k}' for k in range(1, 26)]].sum(axis=1) == \
             df_thi['results']).all()
 
