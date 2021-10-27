e5ec6c4586e6035c51d71813744c250ab2230c36
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-10-27 14:25:30 +0200

    Added parser for WHODAS.

---
+++ evamed/whodas.py
@@ -0,0 +1,39 @@
+"""Processing of WHO Disability Assessment Scale (WHODAS) Evamed
+questionnaires."""
+
+import re
+
+import pandas as pd
+
+
+def _parse_whodas(df, participant):
+    """Parse dataframe and extract WHODAS answers and information."""
+    # clean-up columns
+    columns = [col for col in df.columns if 'WHODAS' in col]
+    assert len(columns) != 0, 'WHODAS not present in dataframe.'
+    prefix = set(col.split('_')[0] for col in columns)
+    assert len(prefix) == 1
+    prefix = list(prefix)[0]
+
+    # locate participant lines
+    df = df.loc[df['patient_code'] == participant]
+
+    # extract information
+    # d-section
+    pattern = re.compile(rf'{prefix}_WHOD\d\d')
+    col_d = [col for col in columns if pattern.match(col)]
+    valid_answers = {'None': 1,
+                     'Mild': 2,
+                     'Moderate': 3,
+                     'Severe': 4,
+                     'Extreme or cannot do': 5}  # to confirm
+    # h-section
+    pattern = re.compile(rf'{prefix}_WHODH\d')
+    col_h = [col for col in columns if pattern.match(col)]
+
+    df_whodas = pd.concat((df[col_d].replace(valid_answers), df[col_h]),
+                          axis=1)
+    df_whodas.insert(0, 'date', pd.to_datetime(df[f'{prefix}_date']))
+    df_whodas.insert(1, 'results', df[f'{prefix}_WHODAS_R'])
+
+    return df_whodas
