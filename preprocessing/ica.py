0af038411c9571cf8d855678c7fb49c0c4c941ce
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-11-08 16:54:03 +0100

    Refactor now obsolete python ICA decomposition.

--- preprocessing/ica.py
+++ preprocessing/ica.py
@@ -3,9 +3,31 @@ import mne
 mne.set_log_level('ERROR')
 
 
-def exclude_EOG_ECG_with_ICA(raw, semiauto=False):
+def _ica(raw, **kwargs):
+    """Fit an ICA with the given kwargs to raw EEG channels.
+    kwargs are passed to mne.preprocessing.ICA()."""
+    ica = mne.preprocessing.ICA(method='picard', max_iter='auto')
+    ica.fit(raw, picks='eeg')
+    return ica
+
+
+def _exclude_ocular_components(raw, ica, **kwargs):
+    """Find and exclude ocular-related components.
+    kwargs are passed to ica.find_bads_eog()."""
+    eog_idx, eog_scores = ica.find_bads_eog(raw, **kwargs)
+    return eog_idx, eog_scores[eog_idx]
+
+
+def _exclude_heartbeat_components(raw, ica, **kwargs):
+    """Find and exclude heartbeat-related components.
+    kwargs are passed to ica.find_bads_ecg()."""
+    ecg_idx, ecg_scores = ica.find_bads_ecg(raw, **kwargs)
+    return ecg_idx, ecg_scores[ecg_idx]
+
+
+def exclude_ocular_and_heartbeat_with_ICA(raw, semiauto=False):
     """
-    Apply ICA to remove EOG and ECG artifacts from raw instance.
+    Apply ICA to remove ocular and heartbeat artifacts from raw instance.
 
     Parameters
     ----------
@@ -19,22 +41,20 @@ def exclude_EOG_ECG_with_ICA(raw, semiauto=False):
     -------
     raw : Raw instance modified in-place.
     ica : ICA instance.
-    eog_scores : Scores used for selection of the EOG component(s).
-    ecg_scores : Scores used for selection of the ECG component(s).
+    eog_scores : Scores used for selection of the ocular component(s).
+    ecg_scores : Scores used for selection of the heartbeat component(s).
     """
-    # ICA decomposition
-    ica = mne.preprocessing.ICA(method='picard', max_iter='auto')
-    ica.fit(raw, picks='eeg')
+    ica = _ica(method='picard', max_iter='auto')
 
-    # EOG
-    eog_idx, eog_scores = ica.find_bads_eog(raw, threshold=0.6,
-                                            measure='correlation')
-    # ECG
-    ecg_idx, ecg_scores = ica.find_bads_ecg(raw, method='correlation',
-                                            threshold=6.6, measure='zscore')
+    eog_idx, eog_scores = \
+        _exclude_ocular_components(raw, ica, threshold=0.6,
+                                   measure='correlation')
+    ecg_idx, ecg_scores = \
+        _exclude_heartbeat_components(raw, ica, method='correlation',
+                                      threshold=6.6, measure='zscore')
 
-    # Exclude occular and heartbeat related components
     ica.exclude = eog_idx + ecg_idx
+
     try:
         assert len(eog_idx) <= 2, 'More than 2 EOG component detected.'
         assert len(ecg_idx) <= 1, 'More than 1 ECG component detected.'
@@ -51,4 +71,4 @@ def exclude_EOG_ECG_with_ICA(raw, semiauto=False):
     ica.apply(raw)
 
     # Scores should not be returned when #9846 is fixed.
-    return raw, ica, eog_scores[eog_idx], ecg_scores[ecg_idx]
+    return raw, ica, eog_scores, ecg_scores
