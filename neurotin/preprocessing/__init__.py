f134d6e2e186726a07b151c94c3bb8165b8160db
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-11-17 19:32:32 +0100

    Add function to apply on raw to __init__

--- neurotin/preprocessing/__init__.py
+++ neurotin/preprocessing/__init__.py
@@ -1,1 +1,10 @@
 """Preprocessing module."""
+
+from .bad_channels import (RANSAC_bads_suggestion,  # noqa: F401
+                           PREP_bads_suggestion)
+from .events import (add_annotations_from_events, check_events,  # noqa: F401
+                     replace_event_value)
+from .filters import apply_filter_aux, apply_filter_eeg  # noqa: F401
+from .ica import exclude_ocular_and_heartbeat_with_ICA  # noqa: F401
+from .meas_info import fill_info, parse_subject_info  # noqa: F401
+from .prepare_raw import prepare_raw  # noqa: F401
