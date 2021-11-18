863c3718f22a20e1878bbcc957a0554262fbf2e1
Mathieu Scheltienne <mathieu.scheltienne@gmail.com>
2021-11-18 15:06:38 +0100

    Fix CLI plot of MML.

--- neurotin/commands/neurotin_logs_mml.py
+++ neurotin/commands/neurotin_logs_mml.py
@@ -1,5 +1,7 @@
 import argparse
 
+from matplotlib import pyplot as plt
+
 from neurotin.logs import plot_mml_across_participants
 
 
@@ -10,10 +12,11 @@ def run():
         description='Analyze minimum masking level .csv file.')
     parser.add_argument(
         'csv', type=str,
-        help='path to the .csv file containing the MML logs.',  required=True)
+        help='path to the .csv file containing the MML logs.')
     parser.add_argument(
-        'participants',
+        '-p',
         help='participant id(s) to include.', nargs='+', required=True)
 
     args = parser.parse_args()
-    plot_mml_across_participants(args.csv, [int(x) for x in args.participants])
+    plot_mml_across_participants(args.csv, [int(x) for x in args.p])
+    plt.show(block=True)
