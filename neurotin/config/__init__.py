a413f1c5566d1c9ddea909641c0160ba689d24c8
Mathieu Scheltienne <73893616+mscheltienne@users.noreply.github.com>
2021-11-23 16:43:10 +0100

    Remove paths.ini

--- neurotin/config/__init__.py
+++ neurotin/config/__init__.py
@@ -1,18 +1,1 @@
 """Config module."""
-
-import socket
-from pathlib import Path
-from configparser import ConfigParser
-
-
-def load_paths():
-    """Load paths stored in paths.ini depending on the system used."""
-    config = ConfigParser(inline_comment_prefixes=('#', ';'))
-    config.optionxform = str
-    config.read(Path(__file__).parent/'paths.ini')
-
-    # find which PC is in use
-    name = socket.gethostname()
-
-    return {key.replace('_', '-').lower(): path
-            for key, path in config.items(name)}
