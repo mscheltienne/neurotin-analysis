6dc89b154952c57f148648b7aee313bb7f934d2e
Mathieu Scheltienne <73893616+mscheltienne@users.noreply.github.com>
2021-12-15 14:13:07 +0100

    add missing import

--- neurotin/evamed/parsers/__init__.py
+++ neurotin/evamed/parsers/__init__.py
@@ -3,5 +3,5 @@
 from .bdi import parse_bdi  # noqa: F401
 from .psqi import parse_psqi  # noqa: F401
 from .stai import parse_stai  # noqa: F401
-from .thi import parse_thi  # noqa: F401
+from .thi import parse_thi, parse_multi_thi  # noqa: F401
 from .whodas import parse_whodas  # noqa: F401
