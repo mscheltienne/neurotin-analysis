20cff96570d22b8a7a70bc8ad7d31fe6fbfff85f
Mathieu Scheltienne <73893616+mscheltienne@users.noreply.github.com>
2022-02-08 10:50:37 +0100

    remove outdated import

--- neurotin/logs/__init__.py
+++ neurotin/logs/__init__.py
@@ -1,6 +1,5 @@
 """Logs module to analyze log files."""
 
-from ._check_participants import _check_participants  # noqa: F401
 from .mml import plot_mml_across_participants  # noqa: F401
 from .scores import (plot_score_evolution_per_participant,  # noqa: F401
                      plot_score_across_participants)
