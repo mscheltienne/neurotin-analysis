2e409ff7d592746f7cb8919e1d844b5f70ee477d
Mathieu Scheltienne <73893616+mscheltienne@users.noreply.github.com>
2022-03-18 17:01:34 +0100

    fix style

--- neurotin/time_frequency/subject/viz.py
+++ neurotin/time_frequency/subject/viz.py
@@ -82,7 +82,7 @@ def align_yaxis(ax1, ax2):
     """Align zeros of the two axes, zooming them out by same ratio"""
     axes = np.array([ax1, ax2])
     extrema = np.array([ax.get_ylim() for ax in axes])
-    tops = extrema[:,1] / (extrema[:,1] - extrema[:,0])
+    tops = extrema[:, 1] / (extrema[:, 1] - extrema[:, 0])
     # Ensure that plots (intervals) are ordered bottom to top:
     if tops[0] > tops[1]:
         axes, extrema, tops = [a[::-1] for a in (axes, extrema, tops)]
@@ -90,6 +90,6 @@ def align_yaxis(ax1, ax2):
     # How much would the plot overflow if we kept current zoom levels?
     tot_span = tops[1] + 1 - tops[0]
 
-    extrema[0,1] = extrema[0,0] + tot_span * (extrema[0,1] - extrema[0,0])
-    extrema[1,0] = extrema[1,1] + tot_span * (extrema[1,0] - extrema[1,1])
+    extrema[0, 1] = extrema[0, 0] + tot_span * (extrema[0, 1] - extrema[0, 0])
+    extrema[1, 0] = extrema[1, 1] + tot_span * (extrema[1, 0] - extrema[1, 1])
     [axes[i].set_ylim(*extrema[i]) for i in range(2)]
