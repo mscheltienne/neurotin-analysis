ce0f04b2fb3c42d08863af60eb1e2e76a63894c6
Mathieu Scheltienne <73893616+mscheltienne@users.noreply.github.com>
2022-03-21 13:05:44 +0100

    remove hard-coded paths

--- scripts/evamed/boxplots_clinical.py
+++ scripts/evamed/boxplots_clinical.py
@@ -9,24 +9,24 @@ from neurotin.io import read_csv_evamed
 # Set participants
 participants = [57, 60, 61, 63, 65, 66, 68, 69, 72, 73]
 # Load clinical dataframes
-fname = r'/Users/scheltie/Desktop/THI.csv'
+fname = r''
 df = read_csv_evamed(fname)
 thi = parse_thi(df, participants)
 thi = thi[thi['visit'].isin(('Baseline', 'Post-assessment'))]
 
-fname = r'/Users/scheltie/Desktop/STAI.csv'
+fname = r''
 df = read_csv_evamed(fname)
 stai = parse_stai(df, participants)
 
-fname = r'/Users/scheltie/Desktop/BDI.csv'
+fname = r''
 df = read_csv_evamed(fname)
 bdi = parse_bdi(df, participants)
 
-fname = r'/Users/scheltie/Desktop/PSQI.csv'
+fname = r''
 df = read_csv_evamed(fname)
 psqi = parse_psqi(df, participants)
 
-fname = r'/Users/scheltie/Desktop/WHODAS.csv'
+fname = r''
 df = read_csv_evamed(fname)
 whodas = parse_whodas(df, participants)
 
