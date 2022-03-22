4e7d64bc029612fe26a9ac97c1c60c6906325f55
Mathieu Scheltienne <73893616+mscheltienne@users.noreply.github.com>
2022-03-22 12:59:19 +0100

    add barplot for evolution of clinical score between baseline and post

--- neurotin/evamed/viz.py
+++ neurotin/evamed/viz.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+import pandas as pd
 import seaborn as sns
 
 from ..utils._checks import _check_type
@@ -28,7 +29,7 @@ def lineplot_evolution(df, name, figsize=(10, 5)):
     ax.set(xlabel="Date", ylabel=f"{name} Score")
 
     # baseline to post-assessment
-    valids = ['Baseline', 'Pre-assessment', 'Post-assessment']
+    valids = ('Baseline', 'Pre-assessment', 'Post-assessment')
     location = df['visit'].isin(valids)
     sns.lineplot(data=df.loc[location], x='date', y='result',
                  hue='participant', palette='muted', markers=False,
@@ -74,5 +75,57 @@ def boxplot_visits(df, name, figsize=(5, 5)):
     order = sorted(df.visit.unique())
     sns.boxplot(x='visit', y='result', data=df, order=order, ax=ax)
     ax.set(xlabel="Visit", ylabel=f"{name} Score (lower is better)")
+
+    return f, ax
+
+
+def barplot_evolution(df, name, figsize=(10, 5)):
+    """Plot the difference Baseline - Post-assessment for each participant
+    as a bar plot.
+
+    Parameters
+    ----------
+    %(df_clinical)s
+    name : str
+        Name of the questionnaire used.
+    %(plt.figsize)s
+
+    Returns
+    -------
+    f : Figure
+    ax : Axes
+    """
+    _check_type(figsize, (tuple, ), item_name='figsize')
+
+    # extract difference between post-assessment and baseline
+    order = ('Baseline', 'Post-assessment')
+    df = df[df['visit'].isin(order)]
+
+    df_dict = {'participant': [], 'result':[]}
+    for participant in df['participant'].unique():
+        df_ = df[df['participant'] == participant]
+        post = df_[df_['visit'] == order[1]]['result'].values[0]
+        base = df_[df_['visit'] == order[0]]['result'].values[0]
+        result = post - base
+
+        # fill dict
+        df_dict['participant'].append(participant)
+        df_dict['result'].append(result)
+
+    df = pd.DataFrame.from_dict(df_dict, orient='columns')
+
+    # order to plot
+    order = [(idx, score) for idx, score in df.values]
+    order = sorted(order, key=lambda x: x[1])
+    order = [int(elt[0]) for elt in order]
+
+    # plot
+    f, ax = plt.subplots(1, 1, figsize=figsize)
+    sns.barplot(x='participant', y='result', data=df, color="steelblue",
+                order=order, ax=ax)
+
+    # set labels
+    ax.set_xlabel('Participant ID')
+    ax.set_ylabel(f'{name} Post - Baseline')
 
     return f, ax
