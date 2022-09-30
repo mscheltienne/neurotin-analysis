349569e5a8a0cd308c8d19e19411fc81f94753b2
mscheltienne <mathieu.scheltienne@fcbg.ch>
2022-09-30 11:06:19 +0200

    fix style

--- neurotin/time_frequency/__init__.py
+++ neurotin/time_frequency/__init__.py
@@ -1,11 +1,11 @@
 """Time-Frequency analysis module."""
 
 from .average import add_average_column  # noqa: F401
+from .band_power import compute_bandpower  # noqa: F401
 from .blocks import (  # noqa: F401
     blocks_count_success,
     blocks_difference_between_consecutive_phases,
 )
-from .band_power import compute_bandpower  # noqa: F401
 from .ratio import ratio  # noqa: F401
 from .weights import (  # noqa: F401
     weights_apply_mask,
