86399d29dcc60b01f8bdeb4f7623e46234e32b6f
mscheltienne <mathieu.scheltienne@fcbg.ch>
2022-11-01 18:13:17 +0100

    add session-level tfr by group of sessions (3 or 5)

--- neurotin/time_frequency/tfr.py
+++ neurotin/time_frequency/tfr.py
@@ -12,6 +12,7 @@ from ..utils._checks import (
     _check_n_jobs,
     _check_participants,
     _check_path,
+    _check_type,
     _check_value,
 )
 from ..utils._docs import fill_doc
@@ -215,6 +216,94 @@ def tfr_session(
         if idx not in results_:
             results_[idx] = dict()
         results_[idx][session] = (tfr, itc)
+    return results_
+
+
+def tfr_session_groupby(
+    folder: Union[str, Path],
+    folder_pp: Union[str, Path],
+    valid_only: bool,
+    participants: Union[int, List[int], Tuple[int, ...]],
+    method: str = "multitaper",
+    n_jobs: int = 1,
+    groupby: int = 5,
+    **kwargs,
+) -> Dict[int, Dict[str, Tuple[AverageTFR, AverageTFR]]]:
+    """Compute the per-session TFR. Sessions are group-by blocks.
+
+    Parameters
+    ----------
+    %(folder_raw_data)s
+    %(folder_pp_data)s
+    %(valid_only)s
+    %(participants)s
+    method : 'multitaper'
+        TFR method used.
+    %(n_jobs)s
+    groupby : int, 3 or 5
+        Number of consecutive session to aggregate together.
+    **kwargs
+        Extra keyword arguments are passed to the TFR method.
+
+    Notes
+    -----
+    'multitaper' requires:
+        - freqs: Frequencies of interest.
+        - n_cycles: Defines the time-resolution as 'T = n_cycles / freqs'
+        - time_bandwith: Defines the frequency-resolution in combination with
+          n_cycles as 'fq_resolution / time_bandwith / T'
+
+    'morlet' requires:
+        - freqs: Frequencies of interest.
+        - n_cycles: Defines the time-resolution as 'T = n_cycles / freqs'
+    """
+    folder = _check_path(folder, item_name="folder", must_exist=True)
+    folder_pp = _check_path(folder_pp, item_name="folder_pp", must_exist=True)
+    participants = _check_participants(participants)
+    n_jobs = _check_n_jobs(n_jobs)
+    _check_value(method, METHODS, "method")
+    _check_type(groupby, ("int",), "groupby")
+    _check_value(groupby, (3, 5), "groupby")
+
+    files = list_runs_pp(
+        folder,
+        folder_pp,
+        participants,
+        valid_only,
+    )
+    input_pool = list()
+    for participant, files_ in files.items():
+        for k, (session, files__) in enumerate(files_.items()):
+            if k == 0:
+                group_files = list()
+                sessions = list()
+            elif k % groupby == 0:
+                input_pool.append(
+                    (group_files, *kwargs.values(), participant, *sessions)
+                )
+                group_files = list()
+                sessions = list()
+            group_files.extend(files__)
+            sessions.append(session)
+        input_pool.append(
+            (group_files, *kwargs.values(), participant, *sessions)
+        )
+
+    assert 0 < len(input_pool)  # sanity check
+    with mp.Pool(processes=n_jobs) as p:
+        results = p.starmap(METHODS[method], input_pool)
+
+    # format results
+    results_ = dict()
+    for res in results:
+        tfr = res[0]
+        itc = res[1]
+        idx = res[2]
+        sessions = res[3:]
+        sessions_str = f"S{'-'.join(str(s)for s in sessions)}"
+        if idx not in results_:
+            results_[idx] = dict()
+        results_[idx][sessions_str] = (tfr, itc)
     return results_
 
 
