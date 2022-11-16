5dca9ffe7c4c80437a60015aacc6b91dacb5a486
mscheltienne <mathieu.scheltienne@fcbg.ch>
2022-11-16 11:15:17 +0100

    add single common plot

--- scripts/srv/model/norm.py
+++ scripts/srv/model/norm.py
@@ -12,6 +12,8 @@ from neurotin.model import (
 
 makedirs(MODEL_FOLDER / "norm")
 dfs = create_weight_dataframe(DATA_FOLDER, PARTICIPANTS)
+
+#%% Individual plots
 for subject, df in dfs.items():
     sum_ = compute_weight_norm_per_session(df)
     f, ax = plt.subplots(1, 1)
@@ -22,3 +24,16 @@ for subject, df in dfs.items():
     ax.yaxis.set_major_formatter(PercentFormatter(1))
     f.tight_layout()
     f.savefig(MODEL_FOLDER / "norm" / f"dist-{str(subject).zfill(3)}.svg")
+    plt.close()
+
+#%% Common plot
+f, ax = plt.subplots(3, 7, figsize=(20, 10), sharex=True, sharey=True)
+for k, (subject, df) in enumerate(dfs.items()):
+    sum_ = compute_weight_norm_per_session(df)
+    ax[k // 7, k % 7].hist(sum_, bins=15, density=True)
+    ax[k // 7, k % 7].set_title(f"Subject {subject}")
+    ax[k // 7, k % 7].yaxis.set_major_formatter(PercentFormatter(1))
+ax[1, 0].set_ylabel("Distribution of the L2 norm / session")
+f.tight_layout()
+f.savefig(MODEL_FOLDER / "norm" / "global.svg")
+plt.close()
