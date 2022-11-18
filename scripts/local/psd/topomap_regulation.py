b367ac7e94595f424c70cb42e848cf16d47a1cd8
mscheltienne <mathieu.scheltienne@fcbg.ch>
2022-11-18 12:39:52 +0100

    clean-up

--- scripts/local/psd/topomap_regulation.py
+++ scripts/local/psd/topomap_regulation.py
@@ -1,57 +1,50 @@
+from pathlib import Path
+
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from mne import create_info
 from mne.viz import plot_topomap
 
-from neurotin.psd import blocks_difference_between_consecutive_phases
+from neurotin.config import PARTICIPANTS
+model_folder = Path('/Users/scheltie/Documents/datasets/neurotin/model/')
 
-#%% Participant
-participants = []
+g1 = [68, 60, 57, 63, 75, 61, 83, 76]
+g2 = [69, 72, 78, 79]
+g3 = [81, 66, 65, 73]
+PARTICIPANTS = g3
 
-#%% Load
-fname = r""
-df = pd.read_pickle(fname)
-diff = blocks_difference_between_consecutive_phases(df, column="all")
+f, ax = plt.subplots(len(PARTICIPANTS), 4, figsize=(8, 20))
 
-#%% Create plots
-f, ax = plt.subplots(2, len(participants), figsize=(20, 4))
+for i, run in enumerate(("full", "regular", "transfer")):
+    fname = f'/Users/scheltie/Documents/datasets/neurotin/bandpower/delta-onrun-{run}-abs.pcl'
+    df = pd.read_pickle(fname)
 
-#%% Compute and plot topographic maps
-electrodes = [
-    col
-    for col in diff.columns
-    if col not in ("participant", "session", "run", "idx", "avg-diff")
-]
-info = create_info([elt.split("-")[0] for elt in electrodes], 1, "eeg")
-info.set_montage("standard_1020")
+    ch_names = [
+        col
+        for col in df.columns
+        if col not in ("participant", "session", "run", "idx")
+    ]
+    info = create_info(ch_names, 1, "eeg")
+    info.set_montage("standard_1020")
 
-for i, participant in enumerate(participants):
-    upreg = {k: np.zeros(len(electrodes)) for k in diff.session.unique()}
-    downreg = {k: np.zeros(len(electrodes)) for k in diff.session.unique()}
+    for k, participant in enumerate(PARTICIPANTS):
+        df_ = df[df["participant"] == participant]
+        df_ = df_[ch_names]
+        data = np.average(df_.values, axis=0)
+        plot_topomap(data - 1, info, axes=ax[k, i+1])
 
-    df_participant = diff[diff["participant"] == participant]
-    sessions = sorted(df_participant["session"].unique())
-    for session in sessions:
-        df_session = df_participant[df_participant["session"] == session]
-        data = df_session[electrodes].values
-        pos = np.argwhere(data > 0)
-        neg = np.argwhere(data < 0)
-        for idx in pos:
-            upreg[session][idx[1]] += data[idx[0], idx[1]]
-        for idx in neg:
-            downreg[session][idx[1]] += data[idx[0], idx[1]]
+for k,participant in enumerate(PARTICIPANTS):
+    model = model_folder / f"{str(participant).zfill(3)}.pcl"
+    model = pd.read_pickle(model)
+    info = create_info(list(model.index), 1, "eeg")
+    info.set_montage("standard_1020")
+    plot_topomap(model.values, info, axes=ax[k, 0])
+    ax[k, 0].set_ylabel(str(participant).zfill(3))
 
-    upreg_ = np.average(np.stack(list(upreg.values())), axis=0)
-    downreg_ = np.average(np.stack(list(downreg.values())), axis=0)
-
-    # plot
-    plot_topomap(upreg_, info, axes=ax[0, i], extrapolate="local", show=False)
-    plot_topomap(
-        downreg_, info, axes=ax[1, i], extrapolate="local", show=False
-    )
-    ax[0, i].set_title(str(participant))
-    ax[0, 0].set_ylabel("Up-regulation")
-    ax[1, 0].set_ylabel("Down-regulation")
-
+ax[0, 0].set_title("Weights")
+ax[0, 1].set_title("Full")
+ax[0, 2].set_title("Regular")
+ax[0, 3].set_title("Transfer")
 f.tight_layout()
+f.savefig("topo-delta-g3.svg")
