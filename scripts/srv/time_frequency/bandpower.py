1e3800abcc4d3b0304bad447554ff3809fc76d16
mscheltienne <mathieu.scheltienne@fcbg.ch>
2022-11-18 12:48:04 +0100

    Revert "re-run rs band power"
    This reverts commit b9536a897d612cb20f6de33b0d152fc681877bbb.

--- scripts/srv/time_frequency/bandpower.py
+++ scripts/srv/time_frequency/bandpower.py
@@ -12,87 +12,87 @@ frequencies = dict(alpha=(8.0, 13.0), delta=(1.0, 4.0))
 n_jobs = 20
 
 
-# #%% full
-# valid_only = True
-# regular_only = False
-# transfer_only = False
+#%% full
+valid_only = True
+regular_only = False
+transfer_only = False
 
-# for band, (fmin, fmax) in frequencies.items():
-#     df_fname = BP_FOLDER / f"{band}-onrun-full.pcl"
-#     df_abs, df_rel = compute_bandpower_onrun(
-#         DATA_FOLDER,
-#         DATA_PP_FOLDER,
-#         valid_only,
-#         regular_only,
-#         transfer_only,
-#         PARTICIPANTS,
-#         fmin,
-#         fmax,
-#         duration,
-#         overlap,
-#         n_jobs=n_jobs,
-#     )
-#     df_abs.to_pickle(
-#         df_fname.with_stem(df_fname.stem + "-abs"), compression=None
-#     )
-#     df_rel.to_pickle(
-#         df_fname.with_stem(df_fname.stem + "-rel"), compression=None
-#     )
+for band, (fmin, fmax) in frequencies.items():
+    df_fname = BP_FOLDER / f"{band}-onrun-full.pcl"
+    df_abs, df_rel = compute_bandpower_onrun(
+        DATA_FOLDER,
+        DATA_PP_FOLDER,
+        valid_only,
+        regular_only,
+        transfer_only,
+        PARTICIPANTS,
+        fmin,
+        fmax,
+        duration,
+        overlap,
+        n_jobs=n_jobs,
+    )
+    df_abs.to_pickle(
+        df_fname.with_stem(df_fname.stem + "-abs"), compression=None
+    )
+    df_rel.to_pickle(
+        df_fname.with_stem(df_fname.stem + "-rel"), compression=None
+    )
 
 
-# #%% regular
-# valid_only = True
-# regular_only = True
-# transfer_only = False
+#%% regular
+valid_only = True
+regular_only = True
+transfer_only = False
 
-# for band, (fmin, fmax) in frequencies.items():
-#     df_fname = BP_FOLDER / f"{band}-onrun-regular.pcl"
-#     df_abs, df_rel = compute_bandpower_onrun(
-#         DATA_FOLDER,
-#         DATA_PP_FOLDER,
-#         valid_only,
-#         regular_only,
-#         transfer_only,
-#         PARTICIPANTS,
-#         fmin,
-#         fmax,
-#         duration,
-#         overlap,
-#         n_jobs=n_jobs,
-#     )
-#     df_abs.to_pickle(
-#         df_fname.with_stem(df_fname.stem + "-abs"), compression=None
-#     )
-#     df_rel.to_pickle(
-#         df_fname.with_stem(df_fname.stem + "-rel"), compression=None
-#     )
+for band, (fmin, fmax) in frequencies.items():
+    df_fname = BP_FOLDER / f"{band}-onrun-regular.pcl"
+    df_abs, df_rel = compute_bandpower_onrun(
+        DATA_FOLDER,
+        DATA_PP_FOLDER,
+        valid_only,
+        regular_only,
+        transfer_only,
+        PARTICIPANTS,
+        fmin,
+        fmax,
+        duration,
+        overlap,
+        n_jobs=n_jobs,
+    )
+    df_abs.to_pickle(
+        df_fname.with_stem(df_fname.stem + "-abs"), compression=None
+    )
+    df_rel.to_pickle(
+        df_fname.with_stem(df_fname.stem + "-rel"), compression=None
+    )
 
-# #%% transfer
-# valid_only = True
-# regular_only = False
-# transfer_only = True
+#%% transfer
+valid_only = True
+regular_only = False
+transfer_only = True
 
-# for band, (fmin, fmax) in frequencies.items():
-#     df_fname = BP_FOLDER / f"{band}-onrun-transfer.pcl"
-#     df_abs, df_rel = compute_bandpower_onrun(
-#         DATA_FOLDER,
-#         DATA_PP_FOLDER,
-#         valid_only,
-#         regular_only,
-#         transfer_only,
-#         PARTICIPANTS,
-#         fmin,
-#         fmax,
-#         duration,
-#         overlap,
-#         n_jobs=n_jobs,
-#     )
-#     df_abs.to_pickle(
-#         df_fname.with_stem(df_fname.stem + "-abs"), compression=None
-#     )
-#     df_rel.to_pickle(
-#         df_fname.with_stem(df_fname.stem + "-rel"), compression=None
-#     )
+for band, (fmin, fmax) in frequencies.items():
+    df_fname = BP_FOLDER / f"{band}-onrun-transfer.pcl"
+    df_abs, df_rel = compute_bandpower_onrun(
+        DATA_FOLDER,
+        DATA_PP_FOLDER,
+        valid_only,
+        regular_only,
+        transfer_only,
+        PARTICIPANTS,
+        fmin,
+        fmax,
+        duration,
+        overlap,
+        n_jobs=n_jobs,
+    )
+    df_abs.to_pickle(
+        df_fname.with_stem(df_fname.stem + "-abs"), compression=None
+    )
+    df_rel.to_pickle(
+        df_fname.with_stem(df_fname.stem + "-rel"), compression=None
+    )
 
 #%% resting-state
 valid_only = True
